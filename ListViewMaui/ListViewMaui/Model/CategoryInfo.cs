8f0490cff9903221afd3a1f4674148809224181a
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2022-05-20 16:43:41 +0530

    Overview blog sample added.

---
+++ ListViewMaui/ListViewMaui/Model/CategoryInfo.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel;
+
+namespace ListViewMaui
+{
+    public class CategoryInfo : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private string name;
+        private string desc;
+        private ImageSource image;
+
+        #endregion
+
+        #region Constructor
+
+        public CategoryInfo()
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string Description
+        {
+            get { return desc; }
+            set
+            {
+                desc = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public ImageSource Image
+        {
+            get
+            {
+                return image;
+            }
+
+            set
+            {
+                image = value;
+                OnPropertyChanged("Image");
+            }
+        }
+
+        #endregion
+
+        #region Interface Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
---
+++ ListViewMaui/ListViewMaui/ViewModel/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ListViewMaui
+{
+public class ViewModel
+{
+    #region Fields
+
+    private ObservableCollection<CategoryInfo> categoryList;
+
+    #endregion
+
+    #region Constructor
+
+    public ViewModel()
+    {
+        GenerateSource();
+    }
+
+    #endregion
+
+    #region Properties
+
+    public ObservableCollection<CategoryInfo> CategoryList
+    {
+        get { return categoryList; }
+        set { this.categoryList = value; }
+    }
+
+    #endregion
+
+    #region Generate Source
+
+    private void GenerateSource()
+    {
+        CategoryInfoRepository categoryinfo = new CategoryInfoRepository();
+        categoryList = categoryinfo.GetCategoryInfo();
+    }
+    #endregion
+}
+}
