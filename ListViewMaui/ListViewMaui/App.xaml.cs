8d5723f8023bbce21027a4a8665dcf97213e9020
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2024-03-28 13:28:43 +0530

    .net 8 sample committed.

--- ListViewMaui/ListViewMaui/App.xaml.cs
+++ ListViewMaui/ListViewMaui/App.xaml.cs
@@ -1,11 +1,12 @@
-﻿namespace ListViewMaui;
-
-public partial class App : Application
+﻿namespace ListViewMaui
 {
-	public App()
-	{
-		InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
 
-		MainPage = new MainPage();
-	}
+            MainPage = new MainPage();
+        }
+    }
 }
